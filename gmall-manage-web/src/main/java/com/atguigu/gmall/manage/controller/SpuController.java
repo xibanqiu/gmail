45a1db5d00656d5770d957241e26e221d4aa5244
xibanqiu <844040829@qq.com>
2021-02-20 09:59:24 +0800

    day03~05 manage project is over ！！add fastFDS

--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/SpuController.java
@@ -5,6 +5,7 @@ import com.atguigu.gmail.bean.PmsProductImage;
 import com.atguigu.gmail.bean.PmsProductInfo;
 import com.atguigu.gmail.bean.PmsProductSaleAttr;
 import com.atguigu.gmail.service.SpuService;
+import com.atguigu.gmall.manage.util.PmsUploadUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,10 +16,8 @@ import java.util.List;
 @CrossOrigin
 public class SpuController {
 
-
     @Reference
     SpuService spuService;
-
 
     @RequestMapping("fileUpload")
     @ResponseBody
@@ -26,7 +25,12 @@ public class SpuController {
         // 将图片或者音视频上传到分布式的文件存储系统
 
         // 将图片的存储路径返回给页面
-        String imgUrl = "https://m.360buyimg.com/babel/jfs/t5137/20/1794970752/352145/d56e4e94/591417dcN4fe5ef33.jpg";
+//        String imgUrl = "https://m.360buyimg.com/babel/jfs/t5137/20/1794970752/352145/d56e4e94/591417dcN4fe5ef33.jpg";
+
+        // 将图片或者音视频上传到分布式的文件存储系统
+        // 将图片的存储路径返回给页面
+        String imgUrl = PmsUploadUtil.uploadImage(multipartFile);
+        System.out.println(imgUrl);
 
         return imgUrl;
     }
